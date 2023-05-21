bca573eb45d6a0d84468833827b975f1e1a66c81
JoseLozano2003 <josecamilo.lozanocet@ucalgary.ca>
2023-05-20 23:15:33 -0600

    website works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,86 @@
-
+import React, { useState, useEffect} from 'react';
+import shuffle from "./utilities/shuffle"
+import Card from "./components/Card"
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	const [cards, setCards] = useState(shuffle)
+	const [pickOne, setPickOne] = useState(null)
+	const [pickTwo, setPickTwo] = useState(null)
+	const [disabled, setDisabled] = useState(false)
+	const [wins, setWins] = useState(0)
+
+	const handleClick = (card) => {
+		if (!disabled) {
+			pickOne ? setPickTwo(card) : setPickOne(card)
+		}
+	}
+
+	const handleTurn = () => {
+		setPickOne(null)
+		setPickTwo(null)
+		setDisabled(false)
+	}
+
+	useEffect(() => {
+		let pickTimer;
+
+		if (pickOne && pickTwo) {
+			if(pickOne.image === pickTwo.image) {
+				setCards((prevCards) =>{
+					return prevCards.map((card) => {
+						if (card.image === pickOne.image){
+							return {...card, matched: true}
+						} else{
+							return card
+						}
+					});
+				});
+				handleTurn();
+			}	else {
+				setDisabled(true)
+				pickTimer = setTimeout(handleTurn, 1000)
+			}
+		}
+		return () => {
+			clearTimeout(pickTimer)
+		}
+	}, [cards, pickOne, pickTwo])
+	
+	useEffect(() => {
+		// Check for any remaining card matches
+		const checkWin = cards.filter((card) => !card.matched);
+	
+		// All matches made, handle win/badge counters
+		if (cards.length && checkWin.length < 1) {
+		  console.log('You win!');
+		  setWins(wins + 1);
+		  handleTurn();
+		  setCards(shuffle);
+		}
+	  }, [cards, wins]);
+
+	return (
+    	<>
+      		<div className="grid">
+			  	{cards.map((card) => {
+          // Destructured card properties
+					const { image, id, matched } = card;
+
+					return (
+						<Card
+						key={id}
+						image={image}
+						selected={card === pickOne || card === pickTwo || matched}
+						onClick={() => {
+							handleClick(card)}
+				
+						}
+						/>
+					);
+				})}
+      		</div>
+    	</>
+  	);
 }
 
 export default App;
---
+++ src/hooks/useAppBadge.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const useAppBadge = () => {
+    const [counter, setCounter] = useState(1);
+
+    const setBadge = () => {
+        
+    }
+}
