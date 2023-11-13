3ef65b7e6436204551e5698ef1306b9d9e4392c0
Jose Lozano <josecamilo.lozanocet@ucalgary.ca>
2023-11-12 20:06:46 -0700

    changed pics

--- src/utilities/shuffle.js
+++ src/utilities/shuffle.js
@@ -1,13 +1,13 @@
 const shuffle = () => {
     const assets = [
-        { image: '/assets2/1.png' },
-        { image: '/assets2/2.png' },
-        { image: '/assets2/3.png'},
-        { image: '/assets2/4.png' },
-        { image: '/assets2/5.png' },
-        { image: '/assets2/6.png' },
-        { image: '/assets2/7.png'},
-        { image: '/assets2/8.png' },
+        { image: '/assets/css.png' },
+        { image: '/assets/dart.png' },
+        { image: '/assets/go.png'},
+        { image: '/assets/js.png' },
+        { image: '/assets/rust.png' },
+        { image: '/assets/react.png' },
+        { image: '/assets/sql.png'},
+        { image: '/assets/node.png' },
     ];
     return [...assets, ...assets]
         .sort(() => Math.random() - 0.5)
